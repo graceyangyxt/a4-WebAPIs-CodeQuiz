1bdd0326d19033de43c92d809dbeee264161737f
Grace Yang <graceyxt.artdesign@gmail.com>
2021-02-24 23:33:25 -0500

    draft1

---
+++ javascript.js
@@ -0,0 +1,26 @@
+var timerEl = document.querySelector(".timer-count");
+var startButton= document.querySelector(".start-button")
+
+
+var timer;
+var timerCount;
+
+
+
+// setAttribute to h1 with questions
+// setAttibute to four button with choices
+
+
+
+// loop in qustion array[]
+
+
+// when interact with the right button, 
+//change to next question, refresh the timer,show correct note
+
+
+//when interact with the wrong button,
+//no respond
+
+
+//
